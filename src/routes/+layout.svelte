6b364feaafaccaa2be45f2a34d5f923450c68f74
Uciel Sola <solauciel@gmail.com>
2023-11-10 17:40:22 -0300

    feat: implement imagekit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,60 @@
 <script>
-	import "../app.css";
-	import "@twicpics/components/style.css";
+	import '../app.css';
 
-	import { installTwicpics } from "@twicpics/components/sveltekit";
+	import { fade } from 'svelte/transition';
 
-	installTwicpics({
-		domain: "https://ucielsola.twic.pics",
-	});
+	import ImageKit from 'imagekit-javascript';
 
-	import { browser } from "$app/environment";
+	import StarsLayer from '$lib/StarsLayer.svelte';
+	import Loader from '$lib/loader.svelte';
 
-	import StarsBackground from "$lib/StarsBackground.svelte";
-
+	let imageURL;
+	let innerHeight;
+	let innerWidth;
 	let loaded = false;
 
-	$: if (browser) {
-		setTimeout(() => {
-			loaded = true;
-		}, 2000);
+	const imagekit = new ImageKit({
+		publicKey: 'public_4fH4SK7wixiXSxgYaDjaaGwjvZE=',
+		urlEndpoint: 'https://ik.imagekit.io/ucielsola',
+	});
+
+	$: if (innerHeight && innerWidth) {
+		imageURL = imagekit.url({
+			path: '/lmot/lmot_bg.png',
+			transformation: [
+				{
+					height: innerHeight + 200,
+					width: innerWidth + 200,
+				},
+			],
+		});
+
+		if (imageURL) {
+			setTimeout(() => (loaded = true), 1500);
+		}
 	}
 </script>
 
-<div class="relative w-screen h-screen overflow-hidden">
-	<div class="absolute top-0 left-0 -z-10">
-		<StarsBackground />
-	</div>
+<svelte:window bind:innerHeight bind:innerWidth />
 
-	{#if loaded}
-		<div class="w-full h-full">
+{#if loaded}
+	<div
+		class="relative w-screen h-screen overflow-hidden bg-fixed bg-center bg-cover"
+		style={`background-image: url('${imageURL}')`}
+		in:fade
+	>
+		<div class="absolute top-0 left-0 z-0 w-screen h-screen">
+			<StarsLayer />
+		</div>
+
+		<div class="w-full h-full overflow-y-auto" in:fade={{ delay: 300 }}>
 			<slot />
 		</div>
-	{/if}
-</div>
+	</div>
+{:else}
+	<div class="flex items-center justify-center w-screen h-screen" out:fade>
+		<StarsLayer />
+
+		<Loader />
+	</div>
+{/if}
