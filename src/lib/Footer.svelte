ae31a9cd81db521f2fada3123353406c25c650db
Uciel Sola <solauciel@gmail.com>
2023-11-12 18:00:10 -0300

    test: see if image changes on scroll

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,5 @@
+<div class="flex items-end justify-center gap-2 py-2 bg-slate-400 h-[34px]">
+	<div class="text-xs border  boreder-slate-900">Acerca de la Orquesta</div>
+
+	<div class="text-xs">Acerca del Show</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,31 @@
 <script>
 	import Header from '$lib/Header.svelte';
+	import Footer from '$lib/Footer.svelte';
 	import InteractiveLine from '$lib/InteractiveLine.svelte';
+	import { fade } from 'svelte/transition';
 
 	let headerHeight;
+	let footerHeight;
 </script>
 
 <div bind:clientHeight={headerHeight}>
-	<div>
-		<Header />
-	</div>
+	<Header />
 
-	<div>
-		<h3
-			class="mx-4 text-xl leading-none tracking-tighter text-center text-white border-b border-white"
-		>
-			SINFONÍA ESTELAR TANGUERA
-		</h3>
-	</div>
+	<h3
+		class="mx-4 text-xl leading-none tracking-tighter text-center text-white border-b border-white"
+	>
+		SINFONÍA ESTELAR TANGUERA
+	</h3>
 </div>
 
-<InteractiveLine {headerHeight} />
+<div
+	class="flex flex-col items-center w-full pb-8 overflow-y-auto"
+	in:fade={{ delay: 500 }}
+	style={`height: calc(100vh - ${headerHeight}px - ${footerHeight}px);`}
+>
+	<InteractiveLine {headerHeight} />
+</div>
+
+<div bind:clientHeight={footerHeight}>
+	<Footer />
+</div>
