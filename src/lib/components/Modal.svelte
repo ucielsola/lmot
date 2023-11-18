1653be231658983139f3c872158d567a941586a8
Uciel Sola <solauciel@gmail.com>
2023-11-18 12:37:45 -0300

    Add close button to Modal component

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -36,6 +36,29 @@
 			out:fade={{ delay: 150 }}
 		/>
 
+		<button
+			on:click={handleClose}
+			class="absolute z-20 top-4 right-4 focus:outline-none"
+			in:fade={{ delay: 150 }}
+			out:fade={{ delay: 0 }}
+		>
+			<span class="sr-only">Close</span>
+			<svg
+				class="w-6 h-6 text-lines"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M6 18L18 6M6 6l12 12"
+				/>
+			</svg>
+		</button>
+
 		<div
 			class="relative z-20 p-2.5 border rounded-md h-min border-{$dataStore.selected
 				.color} bg-modal-bg bg-opacity-40 text-lines"
